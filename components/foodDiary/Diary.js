5b7010e319dc04ba23c0970463b2fbf897d057d7
AlexM141200 <alex.manson14@gmail.com>
2023-03-01 21:27:57 +0000

    Big Fixes

--- components/foodDiary/Diary.js
+++ components/foodDiary/Diary.js
@@ -17,8 +17,10 @@ const Diary = () => {
   const [foodData, setFoodData] = useState(null);
 
   useEffect(() => {
-    fetchFoodData(selectedDate);
-  }, [selectedDate]);
+    if (user) {
+      fetchFoodData(selectedDate);
+    }
+  }, [selectedDate, user]);
 
   const fetchFoodData = async (date) => {
     const userRef = doc(firestore, "users", user.uid);
@@ -34,76 +36,83 @@ const Diary = () => {
   };
 
   return (
-    <div>
-      <h1>Food Diary For Today</h1>
-      <div className="date-picker-container">
-        <button
-          onClick={() =>
-            setSelectedDate(new Date(selectedDate.getTime() - 86400000))
-          }
-        >
-          <i>Previous</i>
-        </button>
-        <DatePicker
-          selected={selectedDate}
-          onChange={(date) => setSelectedDate(date)}
-          dateFormat="yyyy/dd/MM"
-        />
-        <button
-          onClick={() =>
-            setSelectedDate(new Date(selectedDate.getTime() + 86400000))
-          }
-        >
-          <i>Next</i>
-        </button>
-      </div>
-
-      <div className="container">
-        <div className="breakfast">
-          <div className="foodItem">
-            <h1>Breakfast</h1>
-            <div className="foodData" style={{ borderRadius: "10px" }}>
-              {foodData?.breakfast}
-              <AddBreakfast />
-            </div>
-          </div>
-        </div>
-        <div className="lunch">
-          <div className="foodItem">
-            <h1>Lunch</h1>
-            <div className="foodData" style={{ borderRadius: "10px" }}>
-              {foodData?.lunch}
-              <button>Add Entry</button>
-            </div>
-          </div>
-        </div>
-        <div className="dinner">
-          <div className="foodItem">
-            <h1>Dinner</h1>
-            <div className="foodData" style={{ borderRadius: "10px" }}>
-              {foodData?.dinner}
-              <button>Add Entry</button>
-            </div>
-          </div>
-        </div>
-        <div className="snacks">
-          <div className="foodItem">
-            <h1>Snacks</h1>
-            <div
-              className="foodData"
-              style={{ borderRadius: "10px", borderWidth: "2px" }}
+    <>
+      {user ? (
+        <div>
+          <h1>Food Diary For Today</h1>
+          <div className="date-picker-container">
+            <button
+              onClick={() =>
+                setSelectedDate(new Date(selectedDate.getTime() - 86400000))
+              }
             >
-              {foodData?.snacks}
-              <button>Add Entry</button>
+              <i>Previous</i>
+            </button>
+            <DatePicker
+              selected={selectedDate}
+              onChange={(date) => setSelectedDate(date)}
+              dateFormat="yyyy/dd/MM"
+            />
+            <button
+              onClick={() =>
+                setSelectedDate(new Date(selectedDate.getTime() + 86400000))
+              }
+            >
+              <i>Next</i>
+            </button>
+          </div>
+
+          <div className="container">
+            <div className="breakfast">
+              <div className="foodItem">
+                <h1>Breakfast</h1>
+                <div className="foodData" style={{ borderRadius: "10px" }}>
+                  {foodData?.breakfast}
+                  <AddBreakfast />
+                </div>
+              </div>
             </div>
+            <div className="lunch">
+              <div className="foodItem">
+                <h1>Lunch</h1>
+                <div className="foodData" style={{ borderRadius: "10px" }}>
+                  {foodData?.lunch}
+                  <button>Add Entry</button>
+                </div>
+              </div>
+            </div>
+            <div className="dinner">
+              <div className="foodItem">
+                <h1>Dinner</h1>
+                <div className="foodData" style={{ borderRadius: "10px" }}>
+                  {foodData?.dinner}
+                  <button>Add Entry</button>
+                </div>
+              </div>
+            </div>
+            <div className="snacks">
+              <div className="foodItem">
+                <h1>Snacks</h1>
+                <div
+                  className="foodData"
+                  style={{ borderRadius: "10px", borderWidth: "2px" }}
+                >
+                  {foodData?.snacks}
+                  <button>Add Entry</button>
+                </div>
+              </div>
+            </div>
+            <div className="totalCalories">{foodData?.totalCalories}</div>
+            <div className="totalProtein">{foodData?.totalProtein}</div>
+            <div className="totalFat">{foodData?.totalFat}</div>
+            <div className="totalCarbs">{foodData?.totalCarbs}</div>
           </div>
         </div>
-        <div className="totalCalories">{foodData?.totalCalories}</div>
-        <div className="totalProtein">{foodData?.totalProtein}</div>
-        <div className="totalFat">{foodData?.totalFat}</div>
-        <div className="totalCarbs">{foodData?.totalCarbs}</div>
-      </div>
-    </div>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </>
+
   );
 };
 
--- pages/calorieCalculator.js
+++ pages/calorieCalculator.js
@@ -3,7 +3,7 @@ import Calculator from "../components/Calculator";
 const calorieCalculator = () => {
   return (
     <div>
-      <h1>Calorie Calculator</h1>
+      <h1 style={{ textAlign: "center" }}>Calorie Calculator</h1>
       <Calculator />
     </div>
   );
