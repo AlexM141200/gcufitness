d4fab55fb40d8d0e4d8c242c12d26642b39ba78c
AlexM141200 <alex.manson14@gmail.com>
2023-03-14 10:32:51 +0000

    Changed Navbar. Looks much better

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,13 +1,11 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import Router, { useRouter } from "next/router";
-import { Button } from "@nextui-org/react";
-import { Avatar } from "@nextui-org/react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import Login from "./Login.js";
 import { auth } from "../pages/firebase";
-import { Tooltip, Image } from "@nextui-org/react";
 import styles from "../styles/Navbar.module.css";
+import { Navbar, Dropdown, Button, Text, Avatar, Tooltip, Image, useTheme } from "@nextui-org/react";
 
 const NavBar = () => {
   const [user, setUser] = useAuthState(auth);
@@ -37,42 +35,54 @@ const NavBar = () => {
     };
   }, []);
 
-  const userObject = {
-    foodDiary: {},
-    userGoals: {},
-    userWorkouts: {},
-  };
-
-
+  const { isDark } = useTheme();
+  const { width } = useViewport;
+  const isSmallScreen = width < 768;
 
   return (
 
-    <nav>
-      <img className={styles.logo} src="/img/IntelliTech.png" alt="IntelliTech Logo" />
-      <Link href="/" className={activeLink === "/" ? "active" : ""}>
-        Home
-      </Link>
+    <Navbar css={styles} variant="sticky" style={{ top: 0, paddingTop: "0" }} fluid={isSmallScreen}>
+      <Navbar.Brand>
+        <img className={styles.logo} src="/img/IntelliTech.png" alt="IntelliTech Logo" />
+      </Navbar.Brand>
+
       {user ? (
-        <div>
-          <Link
-            href="/foodDiary"
-            className={activeLink === "/foodDiary" ? "active" : ""}
-          >
-            Food Diary
-          </Link>
-          <Link
-            href="/calorieTracker"
-            className={activeLink === "/calorieTracker" ? "active" : ""}
-          >
-            Calorie Tracker
-          </Link>
-          <Link
-            href="/workoutPlanner"
-            className={activeLink === "/workoutPlanner" ? "active" : ""}
-          >
-            Workout Planner
-          </Link>
-        </div>
+        <Navbar.Content
+          enableCursorHighlight
+          activeColor="secondary"
+          hideIn="xs"
+          variant="underline"
+        >
+          <Navbar.Item width={isSmallScreen ? 'auto' : '1/4'}>
+            <Link href="/" className={activeLink === "/" ? "active" : ""}>
+              Home
+            </Link>
+          </Navbar.Item>
+          <Navbar.Item width={isSmallScreen ? 'auto' : '1/4'}>
+            <Link
+              href="/foodDiary"
+              className={activeLink === "/foodDiary" ? "active" : ""}
+            >
+              Food Diary
+            </Link>
+          </Navbar.Item>
+          <Navbar.Item width={isSmallScreen ? 'auto' : '1/4'}>
+            <Link
+              href="/calorieTracker"
+              className={activeLink === "/calorieTracker" ? "active" : ""}
+            >
+              Calorie Tracker
+            </Link>
+          </Navbar.Item>
+          <Navbar.Item width={isSmallScreen ? 'auto' : '1/4'}>
+            <Link
+              href="/workoutPlanner"
+              className={activeLink === "/workoutPlanner" ? "active" : ""}
+            >
+              Workout Planner
+            </Link>
+          </Navbar.Item>
+        </Navbar.Content>
       ) : (
         ""
       )}
@@ -100,7 +110,7 @@ const NavBar = () => {
       ) : (
         ""
       )}
-    </nav>
+    </Navbar>
   );
 };
 export default NavBar;
